e85a972a0a28c87772fd17fab2e7c324d5cbc366
CarlWachter <carlwachter@web.de>
2025-05-26 16:41:38 +0200

    feat(comment): update

--- dist/src/config.js
+++ dist/src/config.js
@@ -214,6 +214,7 @@ async function configFromJobInput() {
     const failOnAlert = getBoolInput('fail-on-alert');
     const alertCommentCcUsers = getCommaSeparatedInput('alert-comment-cc-users');
     let externalDataJsonPath = core.getInput('external-data-json-path');
+    const benchmarkMatrixName = core.getInput('benchmark-matrix-name');
     const maxItemsInChart = getUintInput('max-items-in-chart');
     let failThreshold = getPercentageInput('fail-threshold');
     validateToolType(tool);
@@ -258,6 +259,7 @@ async function configFromJobInput() {
         failOnAlert,
         alertCommentCcUsers,
         externalDataJsonPath,
+        benchmarkMatrixName,
         maxItemsInChart,
         failThreshold,
         ref,
